bfe248204567b95d5141142f462794a630ca3dcd
Claudio Daniel Pacheco Castro <claudiodanielpc@gmail.com>
2021-04-05 13:10:10 -0500

    Update day5.R

--- day5.R
+++ day5.R
@@ -11,7 +11,7 @@ setwd("C:/Users/ALIENWARE/Documents/GitHub/30DayChartChallenge")
 
 #Librerías
 if(!require('pacman')) install.packages('pacman')
-pacman::p_load(tidyverse, extrafont)
+pacman::p_load(tidyverse, extrafont, lubridate)
 
 
 
@@ -27,18 +27,26 @@ arg<-read.csv("https://infra.datos.gob.ar/catalog/sspm/dataset/175/distribution/
 ##Gráfica
 
 arg%>%
-  filter(indice_tiempo>=lubridate::ymd("2015-12-10"))%>%
+  filter(indice_tiempo>=ymd("2003-05-25"))%>%
   #Agregar nombres de presidentes
-  mutate(pres=ifelse(indice_tiempo>=lubridate::ymd("2015-12-10") & 
-                       indice_tiempo<=lubridate::ymd("2019-12-10"), 
-                     "Mauricio Macri","Alberto Fernández"))%>%
+  mutate(pres=ifelse(
+    indice_tiempo>=ymd("2003-05-10") & indice_tiempo<=ymd("2007-12-10"),
+    "Nestor Kirchner",
+    ifelse(indice_tiempo>=ymd("2007-12-11") & indice_tiempo<=ymd("2015-12-09"),
+           "Cristina Fernández de Kirchner",
+    ifelse(
+    indice_tiempo>=ymd("2015-12-10") & 
+                       indice_tiempo<=ymd("2019-12-10"), 
+                     "Mauricio Macri","Alberto Fernández"))))%>%
   ggplot(.,aes(indice_tiempo,dolar_referencia_com_3500, 
                colour=pres))+
   #Puntos
   geom_point(size=0.5)+
   #Colores
   scale_color_manual("Presidente",
-                     values=c("#9ecae1", "#fdae6b"))+
+                     values=c("#9ecae1", "#fdae6b",
+                              "#bdbdbd",
+                              "#31a354"))+
   ##Línea de regresión
   geom_smooth(method = "lm",
               size=1.5)+
@@ -46,7 +54,7 @@ arg%>%
   scale_x_date("Fecha",expand = c(0, 0),
                date_labels ="%Y", date_breaks = "365 days")+
   labs(
-    title = "Argentina. Tipo de cambio diario, 2015-2021",
+    title = "Argentina. Tipo de cambio diario, 2003-2021",
     subtitle = "(pesos por dólar)",
     y= "Pesos por dólar",
     caption = "
@@ -56,11 +64,11 @@ Fuente: @claudiodanielpc con información de Datos Argentina: datos.gob.ar"
   theme(plot.title = element_text(hjust = 0, size=25,face="bold"),
         plot.subtitle = element_text(hjust = 0, size=20, face="italic"),
         plot.caption = element_text(hjust = 0,size=15),
-        legend.position = "bottom",
+        legend.position = "right",
         text=element_text("Tahoma",
                           size=20),
         axis.text.x=element_text(size=12))
 
 
 ##Salvar el gráfico
-ggsave("day5.png",height = 10,width = 30, units="in",dpi=300)
+ggsave("day5.png",height = 10,width = 20, units="in",dpi=300)
